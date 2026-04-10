1d58c7e1d7d8eb4d68a483e2afc14ad0b03be2d9
Methusela11 <soenkilifi.com>
2026-04-10 17:48:47 +0300

    products model

--- backend/config/api/models.py
+++ backend/config/api/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('cakes', 'Cakes'),
+        ('cookies', 'Cookies'),
+        ('breads', 'Breads'),
+        ('chocolates', 'Chocolates'),
+        ('custom', 'Custom'),
+    ]
+
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.TextField()
+
+    def __str__(self):
+        return self.name
