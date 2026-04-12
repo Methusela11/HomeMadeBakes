4f133c04376127784c625677887f25e92b4f085e
Methusela11 <soenkilifi.com>
2026-04-12 11:06:40 +0300

    home chefs

--- frontend/src/pages/BakeryLanding.js
+++ frontend/src/pages/BakeryLanding.js
@@ -8,7 +8,7 @@ import cookiesImage from "../assets/images/cookies/11.png";
 import breadImage from "../assets/images/bread/11.png";
 import chocolatesImage from "../assets/images/chocolates/111.png";
 
-import getrudeImage from "../assets/images/chefs/chef1.png";
+import getrudeImage from "../assets/images/chefs/chef3.png";
 import getrude1Image from "../assets/images/chefs/chef2.png";
 import getrude2Image from "../assets/images/chefs/chef1.png";
 
